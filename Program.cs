4e0cd31f4d40c324585895e7b38ca5cf5c059c9d
dimagarn <sheldingcoder4@gmail.com>
2026-03-19 20:15:14 +0500

    Добавил возможность просматривать список растений и удалять их

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Hangfire;
 using Hangfire.PostgreSql;
+using System.Text;
 
 namespace TelegramQuestBot
 {
@@ -42,9 +43,11 @@ namespace TelegramQuestBot
                         cancellationToken: cancellationToken);
                 }
                 else if (update.Type == UpdateType.Message && update.Message?.Text?.StartsWith("/createplant") == true)
-                {
                     await CreatePlant(update.Message, cancellationToken);
-                }
+                else if (update.Type == UpdateType.Message && update.Message?.Text?.StartsWith("/myplants") == true)
+                    await MyPlants(update.Message.Chat.Id);
+                else if (update.Type == UpdateType.Message && update.Message?.Text?.StartsWith("/deleteplant") == true)
+                    await DeletePlant(update.Message);
             }
 
             async Task CreatePlant(Message message, CancellationToken cancellationToken)
@@ -100,6 +103,49 @@ namespace TelegramQuestBot
                 }
             }
 
+            async Task MyPlants(long chatid)
+            {
+                var plantsList = await dbContext.Plants.Where(p => p.ChatId == chatid).ToListAsync();
+                var stringBuilder = new StringBuilder();
+                if (plantsList.Any())
+                    foreach (var plant in plantsList)
+                        stringBuilder.AppendLine($"🌱 {plant.Name} [#{plant.Id}]\n 💧 Полив: {plant.WateringFrequency}\n 📅 Следующий полив: {plant.NextWateringDate.ToShortDateString()}\n");
+                else
+                {
+                    await bot.SendMessage(chatid, "Похоже, что у тебя еще нет растений, не беда! Используй команду /createplant");
+                    return;
+                }
+
+                await bot.SendMessage(chatid, stringBuilder.ToString());
+            }
+
+            async Task DeletePlant(Message message)
+            {
+                var chatId = message.Chat.Id;
+                var plantId = new int();
+                if (int.TryParse(message.Text.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1], out int plantid))
+                {
+                    plantId = plantid;
+                }
+                else
+                {
+                    await bot.SendMessage(chatId, "ошибка формата!");
+                    return;
+                }
+                var plant = await dbContext.Plants.FindAsync(plantid);
+                if (plant != null && plant.ChatId == chatId)
+                {
+                    dbContext.Plants.Remove(plant);
+                    await dbContext.SaveChangesAsync();
+                    await bot.SendMessage(chatId, $"✅ Растение с id: {plantid} успешно удалено!");
+                } 
+                else
+                {
+                    await bot.SendMessage(chatId, $"❌ Растение с id: {plantid} не найдено!");
+                    return;
+                }
+            }
+
             Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
             {
                 Console.WriteLine($"Произошла ошибка: {exception.Message}");
@@ -119,15 +165,23 @@ namespace TelegramQuestBot
         }
 
         public static async Task SendWateringReminder(int plantid)
+        {
+            using var dbContext = new AppDbContext();
+            var plant = await dbContext.Plants.FindAsync(plantid);
+            if (plant is null)
             {
-                using var dbContext = new AppDbContext();
-                var plant = await dbContext.Plants.FindAsync(plantid);
-                if (plant is null)
-                {
-                    Console.WriteLine("Ошибка, растение не найдено!");
-                    return;
-                }
-                await bot.SendMessage(plant.ChatId, $"Напоминание! Полить растение: {plant.Name} 🌿");
+                Console.WriteLine("Ошибка, растение не найдено!");
+                return;
             }
+            plant.NextWateringDate = DateTime.UtcNow.AddDays(plant.WateringFrequency.ToLower() switch
+                {
+                    "ежедневно" => 1,
+                    "еженедельно" => 7,
+                    "ежемесячно" => 30,
+                    _ => throw new ArgumentException("Invalid watering frequency")
+                });
+            await dbContext.SaveChangesAsync();
+            await bot.SendMessage(plant.ChatId, $"Напоминание! Полить растение: {plant.Name} 🌿");
+        }
     }
 }
